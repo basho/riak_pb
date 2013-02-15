029b02b2fd88efbc97fa5763f7f756cdc0ee72c9
Sean Cribbs <sean@basho.com>
2013-02-15 15:12:01 -0600

    Fix repl enum and add bucket properties codec quickcheck

--- test/encoding_test.erl
+++ test/encoding_test.erl
@@ -86,30 +86,6 @@ pb_test_() ->
      {"msg code encode decode",
       ?_test(begin
                  msg_code_encode_decode(0)
-             end)},
-     {"bucket props encode decode",
-      ?_test(begin
-                 Props = [{n_val, 99},
-                          {allow_mult, true}],
-                 Props2 = riak_pb_codec:decode_bucket_props(
-                            riak_pb:decode_rpbbucketprops(
-                              iolist_to_binary(riak_pb:encode_rpbbucketprops(
-                                riak_pb_codec:encode_bucket_props(Props))))),
-                 MdSame = (lists:sort(Props) =:=
-                               lists:sort(Props2)),
-                 ?assertEqual(true, MdSame)
-             end)},
-     {"bucket props encode decode 2",
-      ?_test(begin
-                 Props = [{n_val, 33},
-                          {allow_mult, false}],
-                 Props2 = riak_pb_codec:decode_bucket_props(
-                            riak_pb:decode_rpbbucketprops(
-                              iolist_to_binary(riak_pb:encode_rpbbucketprops(
-                                riak_pb_codec:encode_bucket_props(Props))))),
-                 MdSame = (lists:sort(Props) =:=
-                               lists:sort(Props2)),
-                 ?assertEqual(true, MdSame)
              end)}
     ].
 
