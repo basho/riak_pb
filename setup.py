26967cdefebdc6183fa0057644bed8cfc5ee36c9
Sean Cribbs <sean@basho.com>
2013-07-31 10:24:53 -0500

    Bump Java and Python versions.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='riak_pb',
-      version='1.4.0.7',
+      version='1.4.1.1',
       description='Riak Protocol Buffers Messages',
       packages=['riak_pb'],
       requires=['protobuf(==2.4.1)'],
