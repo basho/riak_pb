d10c567d66352453a4367de7960f74045646992d
Sean Cribbs <sean@basho.com>
2014-05-30 12:46:44 -0500

    \s -> [[:space:]] fixes the filter

--- tools.mk
+++ tools.mk
@@ -47,9 +47,8 @@ dialyzer-run:
 			exit 1; \
 		fi; \
 		dialyzer $(DIALYZER_FLAGS) --plts $${PLTS} -c ebin > dialyzer_warnings ; \
-		egrep -v "^\s*(done|Checking|Proceeding|Compiling)" dialyzer_warnings | grep -F -f dialyzer.ignore-warnings -v > dialyzer_unhandled_warnings ; \
-		cat dialyzer_warnings ; \
-		cat dialyzer_unhandled_warnings ; wc -c dialyzer_unhandled_warnings ; \
+		egrep -v "^[[:space:]]*(done|Checking|Proceeding|Compiling)" dialyzer_warnings | grep -F -f dialyzer.ignore-warnings -v > dialyzer_unhandled_warnings ; \
+		cat dialyzer_unhandled_warnings ; \
 		[ $$(cat dialyzer_unhandled_warnings | wc -l) -eq 0 ] ; \
 	else \
 		dialyzer $(DIALYZER_FLAGS) --plts $${PLTS} -c ebin; \
