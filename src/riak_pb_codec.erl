af541366361310311dea3cfe3cecc36f8a14e39e
UENISHI Kota <kota@basho.com>
2015-07-21 17:45:06 +0900

    Add dvv_enabled to .proto definition and codec

--- src/riak_pb_codec.erl
+++ src/riak_pb_codec.erl
@@ -189,7 +189,8 @@ decode_bucket_props(#rpbbucketprops{n_val=N,
                                     search_index=Index,
                                     datatype=Datatype,
                                     consistent=Consistent,
-                                    write_once=WriteOnce
+                                    write_once=WriteOnce,
+                                    dvv_enabled=DVVEnabled
                                    }) ->
     %% Extract numerical properties
     [ {P,V} || {P,V} <- [ {n_val, N}, {old_vclock, Old}, {young_vclock, Young},
@@ -200,7 +201,7 @@ decode_bucket_props(#rpbbucketprops{n_val=N,
        {BProp, Bool} <- [{allow_mult, AM}, {last_write_wins, LWW},
                          {basic_quorum, BQ}, {notfound_ok, NFOK},
                          {search, Search}, {consistent, Consistent},
-                         {write_once, WriteOnce}],
+                         {write_once, WriteOnce}, {dvv_enabled, DVVEnabled}],
         Bool /= undefined ] ++
 
     %% Extract commit hooks
@@ -294,6 +295,8 @@ encode_bucket_props([{consistent, S}|Rest], Pb) ->
     encode_bucket_props(Rest, Pb#rpbbucketprops{consistent = encode_bool(S)});
 encode_bucket_props([{write_once, S}|Rest], Pb) ->
     encode_bucket_props(Rest, Pb#rpbbucketprops{write_once = encode_bool(S)});
+encode_bucket_props([{dvv_enabled, Flag} | Rest], Pb) ->
+    encode_bucket_props(Rest, Pb#rpbbucketprops{dvv_enabled = encode_bool(Flag)});
 encode_bucket_props([_Ignore|Rest], Pb) ->
     %% Ignore any properties not explicitly part of the PB message
     encode_bucket_props(Rest, Pb).
