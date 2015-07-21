65f38ae1a601c249c65188e0e8c8e1f2ed277e14
UENISHI Kota <kota@basho.com>
2015-07-21 18:01:14 +0900

    Fix dialyzer mismatch

--- src/riak_pb_codec.erl
+++ src/riak_pb_codec.erl
@@ -349,11 +349,12 @@ encode_commit_hook({struct, Props}=Hook) ->
     end.
 
 %% @doc Converts a list of RpbCommitHook messages into commit hooks.
--spec decode_commit_hooks([ #rpbcommithook{} ]) -> [ commit_hook_property() ].
+-spec decode_commit_hooks([ #rpbcommithook{} ]) -> [ modfun_property() | commit_hook_property() ].
 decode_commit_hooks(Hooks) ->
     [ decode_commit_hook(Hook) || Hook <- Hooks,
                                   Hook =/= #rpbcommithook{modfun=undefined, name=undefined} ].
 
+-spec decode_commit_hook( #rpbcommithook{} ) -> modfun_property() | commit_hook_property().
 decode_commit_hook(#rpbcommithook{modfun = Modfun}) when Modfun =/= undefined ->
     decode_modfun(Modfun, commit_hook);
 decode_commit_hook(#rpbcommithook{name = Name}) when Name =/= undefined ->
