ee73a3d9de496b2aaf6cd77484cb5cb1164c7c7e
Brett Hazen <brett@basho.com>
2015-10-15 16:20:38 +0000

    Tweak python build to work with either Python 2 or 3
    Update erlang_protobuffs to 0.8.2
    
    Changes to make dialyzer happy
    
    Remove everything related to Python, incorporated into riak-python-client
    
    Remove outdated Erlang versions, add more recent one.
    
    Completely ignore generated files

--- riak_pb/__init__.py
+++
@@ -1,6 +0,0 @@
-from riak_pb.riak_dt_pb2 import *
-from riak_pb.riak_kv_pb2 import *
-from riak_pb.riak_pb2 import *
-from riak_pb.riak_search_pb2 import *
-from riak_pb.riak_ts_pb2 import *
-from riak_pb.riak_yokozuna_pb2 import *
