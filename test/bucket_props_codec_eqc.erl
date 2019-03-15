5c5416857e7926a456476bf1c6e6c76409631b84
Thomas Arts <thomas.arts@robuster.software>
2019-03-15 09:18:20 +0100

    export_all in test and QuickCheck mode need no warning

--- test/bucket_props_codec_eqc.erl
+++ test/bucket_props_codec_eqc.erl
@@ -22,7 +22,7 @@
 -include_lib("eqc/include/eqc.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
--compile(export_all).
+-compile([export_all, nowarn_export_all]).
 
 -define(QC_OUT(P), eqc:on_output(fun(F,TL) ->
                                          io:format(user, F, TL)
--- test/encoding_test.erl
+++ test/encoding_test.erl
@@ -1,5 +1,5 @@
 -module(encoding_test).
--compile([export_all]).
+-compile([export_all, nowarn_export_all]).
 -include_lib("eunit/include/eunit.hrl").
 -include("riak_pb_kv_codec.hrl").
 -include("riak_dt_pb.hrl").
